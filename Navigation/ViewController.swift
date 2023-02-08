d77eba592d60312752ba5fe032258bcbd3cc8c8b
Anastasiia Kulikova <av.kulikovaa@yandex.ru>
2023-02-08 22:27:29 +0400

    I've modified file ViewController.swift

--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -2,18 +2,22 @@
 //  ViewController.swift
 //  Navigation
 //
-//  Created by Олег Солодченко on 01.02.2023.
+//  Created by Anastasiia Kulikova on 01.02.2023.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
     }
-
-
+    
+    
+    
+    
 }
 
