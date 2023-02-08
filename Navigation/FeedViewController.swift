75fd59b64418225f380378d4b395a4817a2c3057
Anastasiia Kulikova <av.kulikovaa@yandex.ru>
2023-02-08 22:28:04 +0400

    I've added file FeedViewController.swift

---
+++ Navigation/FeedViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by Anastasiia Kulikova on 08.02.2023.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+
+        var post = Post (title: "My Post")
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .orange
+        
+        let button = UIButton(frame: CGRect(x: 150, y: 650, width: 110, height: 55))
+        button.backgroundColor = .systemBlue
+        button.setTitle("Go Post", for: .normal)
+        button.layer.cornerRadius = 26
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+
+        self.view.addSubview(button)
+      }
+
+      @objc func buttonAction(sender: UIButton!) {
+          let postViewController = PostViewController()
+          self.navigationController?.pushViewController(postViewController, animated: true)
+
+         
+          
+      }
+}
+
+
