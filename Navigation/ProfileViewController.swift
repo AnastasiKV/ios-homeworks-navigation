4cc54ec6beed57da3b2054b9472fa39b20da4c6f
Anastasiia Kulikova <av.kulikovaa@yandex.ru>
2023-02-13 13:21:35 +0400

    Added file Navigation.xcodeproj/project.pbxproj

---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Anastasiia Kulikova on 08.02.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemMint
+
+        
+    }
+
+}
