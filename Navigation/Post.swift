25a85a04cc22a21aaac4514c4e70bca16a524b92
Anastasiia Kulikova <av.kulikovaa@yandex.ru>
2023-03-15 12:44:39 +0400

    Modified struct Post

--- Navigation/Post.swift
+++ Navigation/Post.swift
@@ -5,11 +5,37 @@
 //  Created by Anastasiia Kulikova on 08.02.2023.
 //
 
-import Foundation
+import UIKit
 
 
-struct Post {
+struct PostModel {
     
-    var title: String
+    let author: String
+    let description: String
+    let image: UIImage
+    var likes: Int
+    var views: Int
     
+    
+    static func makeMockPost() -> [[PostModel]] {
+        var examplePost = [[PostModel]]()
+        var section = [PostModel]()
+        section.append(PostModel(author: "av.kulikovaa", description:  "Сегодня был прекрасный закат во время нашего отдыха", image: UIImage(named: "post_1")!, likes: 1543, views: 1346))
+        section.append(PostModel(author: "travel_blog", description: "Самые лучшие места для фотографий в Куале-Лумпуре", image: UIImage(named: "post_2")!, likes: 14322, views: 15279))
+        section.append(PostModel(author: "kotiki_meow", description: "It's so cute", image: UIImage(named: "post_3")!, likes: 25678, views: 27905))
+        section.append(PostModel(author: "vkusvill", description: "Друзья, хотим напомнить, что в каталоге ВкусВилла вы можете найти приятные подарки, цветы, и конечно, торты с праздничным декором", image: UIImage(named: "post_4")!, likes: 4513, views: 5387))
+        for _ in 0...5 {
+           examplePost.append(section)
+        }
+          return examplePost
+
+    }
 }
+
+
+    
+
+        
+    
+
+
