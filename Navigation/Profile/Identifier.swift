dd0c93324bdcc6dbb747200d44d9903b793ce966
Anastasiia Kulikova <av.kulikovaa@yandex.ru>
2023-03-15 12:43:30 +0400

    Adding  image for profile

---
+++ Navigation/Profile/Identifier.swift
@@ -0,0 +1,8 @@
+//
+//  Identifier.swift
+//  Navigation
+//
+//  Created by Олег Солодченко on 14.03.2023.
+//
+
+import Foundation
---
+++ Navigation/Profile/ProfileTableHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileTableHeaderView.swift
+//  Navigation
+//
+//  Created by Олег Солодченко on 13.03.2023.
+//
+
+import UIKit
+
+class ProfileTableHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
